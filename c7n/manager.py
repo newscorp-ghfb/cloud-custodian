9aa1f258aa7e45dd33f9a919d1089745794de2cb
Kent Ou <84220825+kentnsw@users.noreply.github.com>
2022-06-30 23:01:15 +1000

    manager - fix c7n_resource_type_id 4 non-query res

--- c7n/manager.py
+++ c7n/manager.py
@@ -119,7 +119,7 @@ class ResourceManager:
 
         # NOTE annotate resource ID property. moving this to query.py doesn't work.
         for r in resources:
-            if type(r) == dict and "c7n_resource_type_id" not in r:
+            if type(r) == dict and "c7n_resource_type_id" not in r and hasattr(self, "query"):
                 r["c7n_resource_type_id"] = self.get_model().id
 
         self.log.debug("Filtered from %d to %d %s" % (
