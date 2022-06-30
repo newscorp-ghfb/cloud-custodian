5e105b7e3eb6e6fb3afe5aa2aa45273bfc192597
Kent Ou <84220825+kentnsw@users.noreply.github.com>
2022-06-30 12:15:12 +1000

    testing - fix test_aws.py::test_metrics case

--- c7n/resources/aws.py
+++ c7n/resources/aws.py
@@ -261,8 +261,9 @@ class MetricsOutput(Metrics):
                 self.ctx.session_factory).client('cloudwatch', region_name=self.region)
 
         # NOTE filter metrics data by the metric name configured in the policy
-        metrics = [m for m in metrics
-            if m["MetricName"] in self.ctx.policy.data.get("metrics") and m["Value"] > 0]
+        metrics = [m for m in metrics if m["Value"] > 0]
+        if hasattr(self.ctx.policy, "data") and "metrics" in self.ctx.policy.data:
+            metrics = [m for m in metrics if m["MetricName"] in self.ctx.policy.data["metrics"]]
         if not metrics:
             return
 
