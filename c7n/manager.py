d6ac07cdc5fc495b729f4405aa95648ac7195975
Kent Ou <84220825+kentnsw@users.noreply.github.com>
2022-07-01 15:16:00 +1000

    manager - catch c7n_resource_type_id not found

--- c7n/manager.py
+++ c7n/manager.py
@@ -119,8 +119,11 @@ class ResourceManager:
 
         # NOTE annotate resource ID property. moving this to query.py doesn't work.
         for r in resources:
-            if type(r) == dict and "c7n_resource_type_id" not in r and hasattr(self, "query"):
-                r["c7n_resource_type_id"] = self.get_model().id
+            if type(r) == dict and "c7n_resource_type_id" not in r:
+                try:
+                    r["c7n_resource_type_id"] = self.get_model().id
+                except Exception as e:
+                    self.log.warning(f"No resource type id found {str(e)}")
 
         self.log.debug("Filtered from %d to %d %s" % (
             original, len(resources), self.__class__.__name__.lower()))
