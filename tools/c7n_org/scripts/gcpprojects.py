2341eb59b3a0a69e671b4603ff315cbe7316958c
Gianncarlo Giannattasio <giannattasio@newscorp.com>
2024-03-12 16:44:32 -0400

    Added folder option to GCP

--- tools/c7n_org/scripts/gcpprojects.py
+++ tools/c7n_org/scripts/gcpprojects.py
@@ -13,22 +13,38 @@ from c7n_gcp.client import Session
     help="File to store the generated config (default stdout)")
 @click.option('-i', '--ignore', multiple=True,
   help="list of folders that won't be added to the config file")
-def main(output, ignore):
+@click.option('-fold', '--folders', required=False, multiple=True, 
+    help="List folders the will be added to the config file")
+@click.option('-ap','--appscript', default=False, is_flag=True,
+  help="list of app script projects to account files")
+def main(output, ignore, appscript, folders):
     """
     Generate a c7n-org gcp projects config file
     """
-
     client = Session().client('cloudresourcemanager', 'v1', 'projects')
 
     results = []
     for page in client.execute_paged_query('list', {}):
         for project in page.get('projects', []):
 
+            # Exclude App Script GCP Projects
+            if appscript == False:
+                if 'sys-' in project['projectId']:
+                    continue
+                
             if project['lifecycleState'] != 'ACTIVE':
                 continue
 
             if project["parent"]["id"] in ignore:
                 continue
+            
+            if folders != ():
+                if project["parent"]["type"] != "folder":
+                    continue
+                for fold in folders: 
+                    if project["parent"]["id"] != fold:
+                        continue
+
 
             project_info = {
                 'project_id': project['projectId'],
